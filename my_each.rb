a0f1283c638abba6f68294d1a1117af17a2b2688
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 10:26:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  counter = 0
+  while counter < arr.length
+    yield(array[i])
+    counter += 1
+  end
+  return array
 end
+
+my_each(array) { |ele| puts ele }
